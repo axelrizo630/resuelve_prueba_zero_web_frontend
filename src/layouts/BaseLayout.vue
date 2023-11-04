d6aae76ced89cee5e2254a8582fed2be24b7f3f9
axelrizo <axel.rizo@seistreinta.com>
2023-11-04 17:59:25 -0500

    feat(layout): principal layout

--- src/layouts/BaseLayout.vue
+++ src/layouts/BaseLayout.vue
@@ -1,36 +1,49 @@
 <script setup lang="ts">
-import ThePrincipalUserSidebar from "@/components/ThePrincipalUserSidebar.vue";
+import { ref } from "vue";
+import { RouteLocationRaw, useRouter } from "vue-router";
+
+const router = useRouter();
+
+const drawer = ref(false);
+const title = "My App";
+const items = [
+  { title: "Compound Interest Calculation", route: { name: "" } },
+  { title: "Chuck Norris Jokes", route: { name: "" } },
+];
+
+const navigate = (route: RouteLocationRaw) => {
+  router.push(route);
+  drawer.value = false;
+};
+
+const logout = () => {
+  localStorage.removeItem("token");
+  router.push({ name: "Login" });
+};
 </script>
 
 <template>
-  <v-app>
-    <!-- <router-link :to="{ name: 'Home' }">Home</router-link>
-    <router-link :to="{ name: 'About' }">About</router-link> -->
-    <v-container class="pa-0">
-      <v-row>
-        <v-col
-          cols="2"
-          :style="{ minWidth: '88px' }"
-          class="flex-grow-0 flex-shrink-0 sidebar"
-        >
-          <ThePrincipalUserSidebar />
-        </v-col>
-        <v-col class="flex-grow-1">
-          <router-view></router-view>
-        </v-col>
-        <v-col cols="2" class="flex-grow-0 flex-shrink-1 sidebar">
-          <v-sheet class="ma-2 pa-2">
-            I'm 5 column wide and I shrink if there's not enough space
-          </v-sheet>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-app>
-</template>
+  <v-navigation-drawer app v-model="drawer">
+    <v-list>
+      <v-list-item
+        v-for="(item, i) in items"
+        :key="i"
+        @click="navigate(item.route)"
+      >
+        <v-list-item-title>{{ item.title }}</v-list-item-title>
+      </v-list-item>
+      <v-list-item @click="logout">Logout</v-list-item>
+    </v-list>
+  </v-navigation-drawer>
 
-<style scoped>
-.sidebar {
-  position: sticky;
-  top: 0;
-}
-</style>
+  <v-app-bar app>
+    <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-toolbar-title>{{ title }}</v-toolbar-title>
+  </v-app-bar>
+
+  <v-main>
+    <v-container fluid>
+      <router-view-view></router-view-view>
+    </v-container>
+  </v-main>
+</template>
