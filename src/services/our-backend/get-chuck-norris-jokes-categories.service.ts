ebea3800dffae1d47eb595fc61d3486d43f2962b
axelrizo <axel.rizo@seistreinta.com>
2023-11-06 00:39:57 -0500

    fix(dependencies): erase a import that is not used

--- src/services/our-backend/get-chuck-norris-jokes-categories.service.ts
+++ src/services/our-backend/get-chuck-norris-jokes-categories.service.ts
@@ -1,5 +1,4 @@
 import { instance } from "@/services/our-backend/our-backend.instance";
-import { AxiosError } from "axios";
 
 export const getChuckNorrisJokesCategories = async () => {
   const response = await instance.get<string[]>(
